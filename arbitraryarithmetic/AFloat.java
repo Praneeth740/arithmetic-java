c7793844f426764d97360194177044f63600a2fd
Praneeth <cs24btech11013@iith.ac.in>
2025-05-02 10:41:15 +0530

    Added comments in AFloat.java

--- arbitraryarithmetic/AFloat.java
+++ arbitraryarithmetic/AFloat.java
@@ -19,6 +19,7 @@ public class AFloat {
         AFloat S = new AFloat(s);
         return S;
     }
+    //A function to remove dot (1.02->102)
     public String removeDot(String str){
         int i=0;
         while(i<str.length()){
@@ -28,6 +29,7 @@ public class AFloat {
         str=str.substring(0,i)+str.substring(i+1);
         return str;
     }
+    //A function to return the index of '.'
     public int dotAt(String str){
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == '.') return i;
@@ -35,7 +37,7 @@ public class AFloat {
         return -1;
     }
     
-    
+    //A function that adds zeros in the right to make the length of decimal part of both strings to be same.
     public String[] addEndZeros(String str1, String str2) {
         str1 = normalize(str1);
         str2 = normalize(str2);
@@ -60,16 +62,17 @@ public class AFloat {
     
         return new String[]{str1, str2};
     }
-    
+    //A function to normalise various valid strings. (2. -> 2.0)
     private String normalize(String s) {
         if (!s.contains(".")) return s + ".0";
         if (s.endsWith(".")) return s + "0";
         return s;
     }
-    
+    //A function to check if the given string is a valid float.
     public int isFloat(String str) {
+        //Handling null/empty strings.
         if (str == null || str.length() == 0) return 0;
-    
+        //Checking the repetition of '-' more than once in negative inputs.
         int start = 0;
         if (str.charAt(0) == '-') {
             if (str.length() == 1) return 0;
@@ -99,6 +102,7 @@ public class AFloat {
     
         return (digitCount > 0) ? 1 : 0;
     }
+    //A function to check if the string is empty/null.
     public static boolean isZero(String num) {
         if (num == null || num.isEmpty()) return false;
         
@@ -124,6 +128,7 @@ public class AFloat {
         
         return true;
     }
+    //A function to remove to sign. (for easy handling)
     private static String stripSign(String s) {
         return s.startsWith("-") ? s.substring(1) : s;
     }
@@ -141,6 +146,7 @@ public class AFloat {
         }
     
     }
+    //A function to add zeros at the end.
     private static String repeatZeros(int count) {
         String result = "";
         for (int i = 0; i < count; i++) {
@@ -148,18 +154,21 @@ public class AFloat {
         }
         return result;
     }
+    //A method to handle adding various float strings using 'addString' as core logic.
     public String add(String str1,String str2){
+        //Handling invalid inputs.
         if ((isFloat(str1) == 0 && AInteger.isInt(str1) == 0) || 
                         (isFloat(str2) == 0 && AInteger.isInt(str2) == 0)){
             throw new IllegalArgumentException("Inputs must be Floating numbers.");
         }
+        //Padding decimal parts of both strings.
         String[] padded = addEndZeros(str1, str2);
         str1 = padded[0];
         str2 = padded[1];
         int idx=str1.length()-dotAt(str1)-1;
         str1=removeDot(str1);
         str2=removeDot(str2); 
-        
+        //Hadling cases in which one of the inputs is negative.
         if(str1.charAt(0)=='-' && str2.charAt(0)!='-'){
             str1=str1.substring(1);
             str1=AInteger.removeLeadingZeros(str1);
@@ -209,7 +218,9 @@ public class AFloat {
                 return "0.0";
             }
 
-        }else if (str2.charAt(0)=='-' && str1.charAt(0)== '-'){
+        }
+        //Handling cases in which both inputs are negative.
+        else if (str2.charAt(0)=='-' && str1.charAt(0)== '-'){
             str1=str1.substring(1);
             str2=str2.substring(1);
             str1=AInteger.removeLeadingZeros(str1);
@@ -223,7 +234,9 @@ public class AFloat {
             result='-'+result;
             return (AInteger.grtString(result ,"-0")==2) ? "0" :result;
             
-        }else{
+        }
+        //Handling cases in which both inputs are positive.
+        else{
             str1=AInteger.removeLeadingZeros(str1);
             str2=AInteger.removeLeadingZeros(str2);
             String result=AInteger.addString(str1,str1.length(),str2,str2.length());
@@ -236,12 +249,14 @@ public class AFloat {
 
         return "";
     }
+    //A method to handle subracting various float strings using 'subString' as core logic.
     public String sub(String str1, String str2){
+        //Handling invalid inputs.
         if ((isFloat(str1) == 0 && AInteger.isInt(str1) == 0) || 
             (isFloat(str2) == 0 && AInteger.isInt(str2) == 0)) {
             throw new IllegalArgumentException("Inputs must be Floating numbers.");
         }
-    
+        //Padding the decimal part of both the strings.
         String[] padded = addEndZeros(str1, str2);
         str1 = padded[0];
         str2 = padded[1];
@@ -249,7 +264,7 @@ public class AFloat {
     
         str1 = removeDot(str1);
         str2 = removeDot(str2); 
-    
+        //If one of the inputs is negative.
         if(str1.charAt(0) == '-' && str2.charAt(0) != '-') {
             str1 = str1.substring(1);
             str1 = AInteger.removeLeadingZeros(str1);
@@ -273,7 +288,9 @@ public class AFloat {
             result = result.substring(0, result.length() - idx) + "." + result.substring(result.length() - idx);
             return (AInteger.grtString(result, "-0") == 2) ? "0" : result;
     
-        } else if(str2.charAt(0) == '-' && str1.charAt(0) == '-') {
+        } 
+        //if both the inputs are negative.
+        else if(str2.charAt(0) == '-' && str1.charAt(0) == '-') {
             str1 = str1.substring(1);
             str2 = str2.substring(1);
             str1 = AInteger.removeLeadingZeros(str1);
@@ -299,7 +316,9 @@ public class AFloat {
                 return "0.0";
             }
     
-        } else {
+        } 
+        //if both inputs are positive.
+        else {
             str1 = AInteger.removeLeadingZeros(str1);
             str2 = AInteger.removeLeadingZeros(str2);
             if (AInteger.grtString(str1, str2) == 1) {
@@ -326,7 +345,9 @@ public class AFloat {
     
         return "";
     }
+    //A method to handle multipling various float strings using 'mulString' as core logic.
     public String mul(String str1, String str2){
+        //Handling invalid inputs.
         if ((isFloat(str1) == 0 && AInteger.isInt(str1) == 0) || 
             (isFloat(str2) == 0 && AInteger.isInt(str2) == 0)) {
             throw new IllegalArgumentException("Inputs must be Floating numbers.");
@@ -334,7 +355,7 @@ public class AFloat {
     
         str1 = normalize(str1);
         str2 = normalize(str2);
-    
+        //Handling inputs based on their signs.
         boolean neg = false;
         if (str1.charAt(0) == '-' && str2.charAt(0) != '-') {
             neg = true;
@@ -353,7 +374,7 @@ public class AFloat {
         int dec1 = str1.length() - dotAt(str1) - 1;
         int dec2 = str2.length() - dotAt(str2) - 1;
         int totalDecimals = dec1 + dec2;
-    
+        //Removing dot and multiplying them treating as integers.
         str1 = removeDot(str1);
         str2 = removeDot(str2);
     
@@ -365,17 +386,20 @@ public class AFloat {
         while (result.length() <= totalDecimals) {
             result = "0" + result;
         }
-    
+        //Inserting the '.' in correct position of product.
         result = result.substring(0, result.length() - totalDecimals) + "." + result.substring(result.length() - totalDecimals);
-    
+        //Adding '-' if neccessary.
         if (neg && !result.equals("0.0")) result = "-" + result;
     
         return result;
     }
+    //A method to handle dividing various float strings using 'divString' as core logic.
     public static String div(String str1, String str2) {
+        //Handling invalid inputs.
         if (isZero(str2)){
             throw new ArithmeticException("Division by zero");
         }
+        //Handling inputs with various signs.
         boolean negative = (str1.startsWith("-") != str2.startsWith("-")) 
             && !isZero(str1);
         str1 = stripSign(str1);
@@ -384,14 +408,14 @@ public class AFloat {
         int[] decDivisor = countDecimals(str2);
         String num1 = removeDecimal(str1);
         String num2 = removeDecimal(str2);
-        int precision = 30;
+        int precision = 30;    //Precision.
         num1 = num1 + repeatZeros(precision + decDivisor[1]);
         String quotient = AInteger.div(num1, num2);
         int decimalShift = decDividend[1] + decDivisor[1];
-        int dotPos = quotient.length() - decimalShift - precision;
-
+        int dotPos = quotient.length() - decimalShift - precision; //calculating where '.' should sit in quotient.
+        
         StringBuilder result = new StringBuilder();
-        if (dotPos <= 0) {
+        if (dotPos <= 0) {                                 //If the quotient is purely fractional
             result.append("0.");
             result.append(repeatZeros(-dotPos));
             result.append(quotient);
